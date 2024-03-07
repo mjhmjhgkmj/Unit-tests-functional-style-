a14b680320433347337cdc9a1e25495520b4811e
дмитрий козлов <hance@yandex.ru>
2024-03-07 19:27:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/PositiveNumberExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CSharpFunctionalExtensions;
+namespace PositiveNumber;
+public static class PositiveNumberExtensions
+{
+    public static Result<PositiveNumber<T>, string> ToPositiveNumber<T>(this T value) where T : struct, IComparable<T> =>
+        ! (typeof(T) == typeof(double) || typeof(T) == typeof(float) || typeof(T) == typeof(decimal) || typeof(T) == typeof(int))
+        ? Result.Failure<PositiveNumber<T>, string>($"{typeof(T)} is not a Real number")
+        : value.CompareTo(default) <= 0
+            ? Result.Failure<PositiveNumber<T>, string>("Value must be positive")
+            : Result.Success<PositiveNumber<T>, string>(new PositiveNumber<T>(value));
+
+    public static Result<double, string> SquareRoot<T>(this Result<PositiveNumber<T>, string> result) where T : struct, IComparable<T> =>
+         result.Map(positiveNumber => Math.Sqrt(Convert.ToDouble(positiveNumber.Value)));
+    public record PositiveNumber<T>(T Value) where T : struct, IComparable<T>;
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,10 @@
+﻿using PositiveNumber;
+public static class Program
+{
+    public static void Main()
+    {
+        // example
+        var result = 'd'.ToPositiveNumber().SquareRoot();
+        Console.WriteLine(result);
+    }
+}
